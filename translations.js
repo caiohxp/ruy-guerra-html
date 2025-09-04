02a7378c9205e15d1c38320c87d1fd4e41beea8c
caiohxp <caiocesar.ccomp@gmail.com>
2025-09-04 01:59:18 -0300

    up: cronologia e trabalhos, dropdown filmografia

--- translations.js
+++ translations.js
@@ -13,7 +13,56 @@ const translations = {
         nav_around_the_world: "Pelo Mundo",
         nav_contact: "Contato",
         works_about_title: "Trabalhos Sobre",
-        works_about_content: "Conteúdo sobre trabalhos em breve...",
+        works_about_content: `
+<ul class="teses">
+    <li>BORGES, Vavy Pacheco: “Ruy Guerra: uma vida à medida do sonho”, publicação, Festival do Cine Documentário, Maputo, Moçambique, 2011</li>
+    <li>BORGES, Vavy Pacheco: “Nas pegadas de um leão” em Grafia da vida: reflexões e experiências com a escrita biográfica, Alexandre de Sá Avelar e Benito Bissio (orgs.), Editora Letra e Voz, SP 2012, pp. 83-104</li>
+    <li>BORGES, Vavy Pacheco: “Paixão Escancarada - Ruy Guerra, vida e obra” (biografia em fase de finalização)</li>
+</ul>
+<br>
+<p class="imagem">
+    <img class="img350" src="images/biografia_ruy_guerra.jpg" alt="biografia de Ruy Guerra por Vavy Pacheco Borges" title="biografia de Ruy Guerra por Vavy Pacheco Borges" />
+</p>
+<br>
+<ul class="teses">
+    <li>BRISENO, Marcelo: “Recursos Cinematográficos de Estorvo no contexto pós-moderno”, artigo em revista INOVCOM, Vol.1, No 2, 2006</li>
+    <li>CONTI, Maria Aparecida. Calabar, o elogio da traição: drama da memória ou trama na história? 2007. 119 f. Dissertação (Mestrado em Linguística) – Universidade Federal de Uberlândia, Uberlândia, 2007</li>
+    <li>DOURADO, Elziane O.: O diálogo entre o cinema direto e o filme A Queda, Cinema e ciências humanas, Multimeios UNICAMP, Campinas, s.d.</li>
+    <li>DUARTE, Mimoso. Ruys, Imagologies et Métissage: Enjeu de l’espace littéraire et filmique: des scénarios et des “Cuentos” 1972 de Gabriel García Márquez à Erendira (1982, un film de Ruy Guerra), Littérature Comparée et Cinema, Université de Toulouse II, Toulouse, 2001</li>
+    <li>GARCIA, Miliandre: “A luta agora é na justiça: o processo censório de Calabar”, artigo em revista PolHis ano 5, Número 9, primeiro semestre 2012</li>
+    <li>HARDMANN, Halina: “Os Cafajestes e Os Fuzis: proximidade e distância na representação popular”, ensaio de avaliação, UFSC, Florianópolis, 2008</li>
+    <li>MARTINS, Christian Alves. Diálogos entre passado e presente: “Calabar, O elogio da traição” (1973) de Chico Buarque e Ruy Guerra. 2007. 201 f. Dissertação (Mestrado em História) – Programa de Pós-Graduação em História, Universidade Federal de Uberlândia, Uberlândia, 2007</li>
+    <li>MARTINS, Christian Alves. Rupturas e permanências: a recepção de “Calabar, o elogio da traição” de Chico Buarque e Ruy Guerra. 2013. 230 f. Tese (Doutorado em História Social) – Programa de Pós-Graduação em História do Instituto de História, Universidade Federal de Uberlândia, Uberlândia, 2013.</li>
+    <li>MEDEIROS, Viviane. Rupturas e descaminhos: o Estorvo da pós-modernidade, Monografia: Imagens e representações da cidade, PUC-RJ, Rio de Janeiro, 2003</li>
+    <li>MELO, Pedro Henrique de: “O riso: a linguagem da transgressão através da leitura de Calabar - o elogio da transgressão de Chico Buarque e Ruy Guerra”, especialização em Letras, UFOP, 2005</li>
+    <li>REIS, Mirian S. Carneiro. Náusea e absurdo: o existencialismo em Estorvo, Mestrado em Literatura e diversidade cultural, Universidade Estadual de Feira de Santana, Bahia, 2010</li>
+    <li>NUNES, Elzimar F.: “A reescrita da História em Calabar, o elogio da traição, de Chico Buarque e Ruy Guerra”, Universidade de Brasília, 2002</li>
+    <li>PINTO, Carlos Eduardo Pinto de. Imaginar a cidade real: o Cinema Novo e a representação da modernidade urbana carioca (1955-1970). 2013. 344 f. Tese (doutorado em História) – Programa de Pós-Graduação em História, Universidade Federal Fluminense (UFF), Niterói, 2013</li>
+    <li>PINTO, Carlos Eduardo Pinto de. Câmera-arma: a representação das funções sociais da fotografia em Os Cafajestes (Ruy Guerra, 1962). Revista Brasileira de História da Mídia, Porto Alegre/São Paulo, v. 2, n. 2, jul./dez., p. 151-158, 2013</li>
+    <li>PINTO, Leonor Stella de Souza. Le cinéma brésilien au risque de la censure pendant la dictature militaire de 1964 à 1985. Thèse de Doctorat, Univ de Toulouse “Le Mirail”, 2001</li>
+    <li>PORTANOVA DE BARROS, Eduardo: “O nomadismo poético em Estorvo, de Ruy Guerra”, comunicação no: IX Seminário Internacional da Comunicação: Simulacros e dis(simulações) na sociedade hiperespetacular (Anais). Porto Alegre: EDIPUCRS, 2007</li>
+    <li>PORTANOVA DE BARROS, Eduardo: “O cinema de Ruy Guerra; um imaginário autoral na pós-modernidade”, Tese de Doutorado, PUC-RS, 2009</li>
+    <li>ROCHA, Elizabete Sanches. O elogio da liberdade: procedimentos estéticos em Calabar. 2003. 299 f. Tese (Doutorado em letras) – Faculdade de Ciência e Letras, Universidade Estadual Paulista Júlio de Mesquita Filho, Araraquara, 2003</li>
+    <li>SCHEFER, Raquel. Birth of fiction: On "Mueda, Memória e Massacre", by Ruy Guerra. Africa's Lost Classics: New Histories of African Cinema/ ed. por Lizelle Bisschoff e David Murphy. Oxford: Legenda, Maney Publishing, 9 p., no prelo (Outubro de 2014)</li>
+    <li>SCHEFER, Raquel. La forme-événement: Le Cinéma Révolutionnaire Mozambicain et le Cinéma de Libération, Thèse de Doctorat d’études Cinématographiques à l’Université Sorbonne Nouvelle-Paris 3 (Outubro, 2015)</li>
+    <li>SCHEFER, Raquel. Reconstitutions / Autour de “Mueda, Memória e Massacre” de Ruy Guerra. Article commenté. 10 ans de Bétonsalon – Centre d’art et de recherche/ ed. por Mélanie Bouteloup. Paris: Bétonsalon e Les Éditions Beaux-arts, no prelo (Setembro de 2014)</li>
+    <li>SCHEFER, Raquel. Fictions of the Liberation Struggle: Ruy Guerra, José Cardoso, Zdravko Velimirovic. Assubuji, Rui, Israel, Paolo e Thompson, Drew (ed. por), The Liberation Script in Mozambican History, Kronos: Southern African Histories, XXXIX, Novembro de 2013, pp. 298-315</li>
+    <li>SCHEFER, Raquel. Re-constitutions. On “Mueda, Memória e Massacre”, by Ruy Guerra. Bouteloup, Mélanie (ed. por), You do not stand in one place to watch a masquerade, Le Journal de La Triennale, Paris: CNAP, Palais de Tokyo, IV, 2012, pp. 39-41<br>
+        <a href="http://www.cnap.fr/sites/default/files/article/123910_le-journal-de-la-triennale--4--on_ne_reste_pas_au_meme_endroit--melaniebouteloup_adm2.pdf">URL</a>
+    </li>
+    <li>SCHEFER, Raquel. O Nascimento de uma Imagem, Buala, 2011<br>
+        <a href="http://www.buala.org/pt/afroscreen/o-nascimento-de-uma-imagem-mueda-memoria-e-massacre-de-ruy-guerra-1979">URL</a>
+    </li>
+    <li>SILVA, Anderson Lopes e RIBEIRO, Regiane Regina: “O cinema brasileiro e as identidades híbridas em Ruy Guerra: o olhar latino-africano de um cidadão da vida”, artigo, Journal of Brazilian Studies Vol 3 Nro 1 (Jul.2014)</li>
+    <li>SILVA, Anderson Lopes. “O cinema autoral em Ruy Guerra: uma análise fílmica de ‘Os Cafajestes’ (1962)”. 2013. Monografia (Especialização em Comunicação, Cultura e Arte) - Pontifícia Universidade Católica do Paraná, Curitiba</li>
+    <li>SILVA, Anderson Lopes. “Revisitando Ruy Guerra: um percurso metodológico pela análise fílmica de 'Os Cafajestes' (1962)”, 'Toma Uno', do Departamento de Cinema e Televisão (Universidad Nacional de Córdoba, Argentina) (no prelo)</li>
+    <li>SOUZA, Maria Cristina R. da C. O discurso de oposição de Chico Buarque de Hollanda na ditadura militar brasileira. 1999. 92 f. Dissertação (Mestrado em Língua Portuguesa) – Pontifícia Universidade Católica de São Paulo, São Paulo, 1999</li>
+    <li>TAVARES, Ane Carolina Randig. “(Re)criar para (re)contar: O processo transcriativo de La mala hora, de Gabriel García Márquez, por Ruy Guerra". Dissertação de mestrado (Estudos de Literatura - PPGlit) novembro de 2013, UFSCAR</li>
+    <li>VIANA, Ana Cristina Caminha. Calabar, O Elogio da Traição: Um novo drama histórico. 2006. 123 f. Dissertação (Mestrado em Letras) – Universidade Federal do Ceará, Fortaleza, 2006</li>
+    <li>XAVIER, Ismail: O olho mágico, o abrigo e a ameaça: Ruy Guerra filma Chico Buarque, em MATRIZes, ECA-USP, Capa, vol.2, No 2, 2009</li>
+    <li>XAVIER, Ismail: “Os deuses e os mortos: Maldição dos deuses ou maldição da História”, artigo, Ilha do Desterro, Florianópolis No 32, 1997</li>
+</ul>
+`,
         archive_title: "Acervo",
         archive_content: "Conteúdo do acervo em breve...",
         home_p1: `Ruy Guerra nasceu a 22/08/1931 em <span class="text-highlight">Moçambique</span>, colónia africana portuguesa às margens do Oceano Índico. Desde jovem foi um apaixonado pela palavra e pela imagem. Escreveu poesias, contos, críticas cinematográficas para a imprensa da capital Lourenço Marques; assíduo frequentador das matinés de cinema, filmou documentário sobre o cotidiano dos trabalhadores das docas desse porto importante da África Austral.`,
@@ -272,7 +321,56 @@ const translations = {
         chronology_title: "Chronology",
         chronology_content: "Chronology content coming soon...",
         works_about_title: "Works About",
-        works_about_content: "Content about works coming soon...",
+        works_about_content: `
+<ul class="teses">
+    <li>BORGES, Vavy Pacheco: “Ruy Guerra: uma vida à medida do sonho”, publicação, Festival do Cine Documentário, Maputo, Moçambique, 2011</li>
+    <li>BORGES, Vavy Pacheco: “Nas pegadas de um leão” em Grafia da vida: reflexões e experiências com a escrita biográfica, Alexandre de Sá Avelar e Benito Bissio (orgs.), Editora Letra e Voz, SP 2012, pp. 83-104</li>
+    <li>BORGES, Vavy Pacheco: “Paixão Escancarada - Ruy Guerra, vida e obra” (biografia em fase de finalização)</li>
+</ul>
+<br>
+<p class="imagem">
+    <img class="img350" src="images/biografia_ruy_guerra.jpg" alt="biografia de Ruy Guerra por Vavy Pacheco Borges" title="biografia de Ruy Guerra por Vavy Pacheco Borges" />
+</p>
+<br>
+<ul class="teses">
+    <li>BRISENO, Marcelo: “Recursos Cinematográficos de Estorvo no contexto pós-moderno”, artigo em revista INOVCOM, Vol.1, No 2, 2006</li>
+    <li>CONTI, Maria Aparecida. Calabar, o elogio da traição: drama da memória ou trama na história? 2007. 119 f. Dissertação (Mestrado em Linguística) – Universidade Federal de Uberlândia, Uberlândia, 2007</li>
+    <li>DOURADO, Elziane O.: O diálogo entre o cinema direto e o filme A Queda, Cinema e ciências humanas, Multimeios UNICAMP, Campinas, s.d.</li>
+    <li>DUARTE, Mimoso. Ruys, Imagologies et Métissage: Enjeu de l’espace littéraire et filmique: des scénarios et des “Cuentos” 1972 de Gabriel García Márquez à Erendira (1982, un film de Ruy Guerra), Littérature Comparée et Cinema, Université de Toulouse II, Toulouse, 2001</li>
+    <li>GARCIA, Miliandre: “A luta agora é na justiça: o processo censório de Calabar”, artigo em revista PolHis ano 5, Número 9, primeiro semestre 2012</li>
+    <li>HARDMANN, Halina: “Os Cafajestes e Os Fuzis: proximidade e distância na representação popular”, ensaio de avaliação, UFSC, Florianópolis, 2008</li>
+    <li>MARTINS, Christian Alves. Diálogos entre passado e presente: “Calabar, O elogio da traição” (1973) de Chico Buarque e Ruy Guerra. 2007. 201 f. Dissertação (Mestrado em História) – Programa de Pós-Graduação em História, Universidade Federal de Uberlândia, Uberlândia, 2007</li>
+    <li>MARTINS, Christian Alves. Rupturas e permanências: a recepção de “Calabar, o elogio da traição” de Chico Buarque e Ruy Guerra. 2013. 230 f. Tese (Doutorado em História Social) – Programa de Pós-Graduação em História do Instituto de História, Universidade Federal de Uberlândia, Uberlândia, 2013.</li>
+    <li>MEDEIROS, Viviane. Rupturas e descaminhos: o Estorvo da pós-modernidade, Monografia: Imagens e representações da cidade, PUC-RJ, Rio de Janeiro, 2003</li>
+    <li>MELO, Pedro Henrique de: “O riso: a linguagem da transgressão através da leitura de Calabar - o elogio da transgressão de Chico Buarque e Ruy Guerra”, especialização em Letras, UFOP, 2005</li>
+    <li>REIS, Mirian S. Carneiro. Náusea e absurdo: o existencialismo em Estorvo, Mestrado em Literatura e diversidade cultural, Universidade Estadual de Feira de Santana, Bahia, 2010</li>
+    <li>NUNES, Elzimar F.: “A reescrita da História em Calabar, o elogio da traição, de Chico Buarque e Ruy Guerra”, Universidade de Brasília, 2002</li>
+    <li>PINTO, Carlos Eduardo Pinto de. Imaginar a cidade real: o Cinema Novo e a representação da modernidade urbana carioca (1955-1970). 2013. 344 f. Tese (doutorado em História) – Programa de Pós-Graduação em História, Universidade Federal Fluminense (UFF), Niterói, 2013</li>
+    <li>PINTO, Carlos Eduardo Pinto de. Câmera-arma: a representação das funções sociais da fotografia em Os Cafajestes (Ruy Guerra, 1962). Revista Brasileira de História da Mídia, Porto Alegre/São Paulo, v. 2, n. 2, jul./dez., p. 151-158, 2013</li>
+    <li>PINTO, Leonor Stella de Souza. Le cinéma brésilien au risque de la censure pendant la dictature militaire de 1964 à 1985. Thèse de Doctorat, Univ de Toulouse “Le Mirail”, 2001</li>
+    <li>PORTANOVA DE BARROS, Eduardo: “O nomadismo poético em Estorvo, de Ruy Guerra”, comunicação no: IX Seminário Internacional da Comunicação: Simulacros e dis(simulações) na sociedade hiperespetacular (Anais). Porto Alegre: EDIPUCRS, 2007</li>
+    <li>PORTANOVA DE BARROS, Eduardo: “O cinema de Ruy Guerra; um imaginário autoral na pós-modernidade”, Tese de Doutorado, PUC-RS, 2009</li>
+    <li>ROCHA, Elizabete Sanches. O elogio da liberdade: procedimentos estéticos em Calabar. 2003. 299 f. Tese (Doutorado em letras) – Faculdade de Ciência e Letras, Universidade Estadual Paulista Júlio de Mesquita Filho, Araraquara, 2003</li>
+    <li>SCHEFER, Raquel. Birth of fiction: On "Mueda, Memória e Massacre", by Ruy Guerra. Africa's Lost Classics: New Histories of African Cinema/ ed. por Lizelle Bisschoff e David Murphy. Oxford: Legenda, Maney Publishing, 9 p., no prelo (Outubro de 2014)</li>
+    <li>SCHEFER, Raquel. La forme-événement: Le Cinéma Révolutionnaire Mozambicain et le Cinéma de Libération, Thèse de Doctorat d’études Cinématographiques à l’Université Sorbonne Nouvelle-Paris 3 (Outubro, 2015)</li>
+    <li>SCHEFER, Raquel. Reconstitutions / Autour de “Mueda, Memória e Massacre” de Ruy Guerra. Article commenté. 10 ans de Bétonsalon – Centre d’art et de recherche/ ed. por Mélanie Bouteloup. Paris: Bétonsalon e Les Éditions Beaux-arts, no prelo (Setembro de 2014)</li>
+    <li>SCHEFER, Raquel. Fictions of the Liberation Struggle: Ruy Guerra, José Cardoso, Zdravko Velimirovic. Assubuji, Rui, Israel, Paolo e Thompson, Drew (ed. por), The Liberation Script in Mozambican History, Kronos: Southern African Histories, XXXIX, Novembro de 2013, pp. 298-315</li>
+    <li>SCHEFER, Raquel. Re-constitutions. On “Mueda, Memória e Massacre”, by Ruy Guerra. Bouteloup, Mélanie (ed. por), You do not stand in one place to watch a masquerade, Le Journal de La Triennale, Paris: CNAP, Palais de Tokyo, IV, 2012, pp. 39-41<br>
+        <a href="http://www.cnap.fr/sites/default/files/article/123910_le-journal-de-la-triennale--4--on_ne_reste_pas_au_meme_endroit--melaniebouteloup_adm2.pdf">URL</a>
+    </li>
+    <li>SCHEFER, Raquel. O Nascimento de uma Imagem, Buala, 2011<br>
+        <a href="http://www.buala.org/pt/afroscreen/o-nascimento-de-uma-imagem-mueda-memoria-e-massacre-de-ruy-guerra-1979">URL</a>
+    </li>
+    <li>SILVA, Anderson Lopes e RIBEIRO, Regiane Regina: “O cinema brasileiro e as identidades híbridas em Ruy Guerra: o olhar latino-africano de um cidadão da vida”, artigo, Journal of Brazilian Studies Vol 3 Nro 1 (Jul.2014)</li>
+    <li>SILVA, Anderson Lopes. “O cinema autoral em Ruy Guerra: uma análise fílmica de ‘Os Cafajestes’ (1962)”. 2013. Monografia (Especialização em Comunicação, Cultura e Arte) - Pontifícia Universidade Católica do Paraná, Curitiba</li>
+    <li>SILVA, Anderson Lopes. “Revisitando Ruy Guerra: um percurso metodológico pela análise fílmica de 'Os Cafajestes' (1962)”, 'Toma Uno', do Departamento de Cinema e Televisão (Universidad Nacional de Córdoba, Argentina) (no prelo)</li>
+    <li>SOUZA, Maria Cristina R. da C. O discurso de oposição de Chico Buarque de Hollanda na ditadura militar brasileira. 1999. 92 f. Dissertação (Mestrado em Língua Portuguesa) – Pontifícia Universidade Católica de São Paulo, São Paulo, 1999</li>
+    <li>TAVARES, Ane Carolina Randig. “(Re)criar para (re)contar: O processo transcriativo de La mala hora, de Gabriel García Márquez, por Ruy Guerra". Dissertação de mestrado (Estudos de Literatura - PPGlit) novembro de 2013, UFSCAR</li>
+    <li>VIANA, Ana Cristina Caminha. Calabar, O Elogio da Traição: Um novo drama histórico. 2006. 123 f. Dissertação (Mestrado em Letras) – Universidade Federal do Ceará, Fortaleza, 2006</li>
+    <li>XAVIER, Ismail: O olho mágico, o abrigo e a ameaça: Ruy Guerra filma Chico Buarque, em MATRIZes, ECA-USP, Capa, vol.2, No 2, 2009</li>
+    <li>XAVIER, Ismail: “Os deuses e os mortos: Maldição dos deuses ou maldição da História”, artigo, Ilha do Desterro, Florianópolis No 32, 1997</li>
+</ul>
+`,
         archive_title: "Archive",
         archive_content: "Archive content coming soon...",
         home_p1: `Ruy Guerra was born on August 22, 1931, in <span class="text-highlight">Mozambique</span>...`,
